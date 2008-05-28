f08b861ed08914e317aa657418b8a1b4ef191b2d
James Tauber <jtauber@jtauber.com>
2008-05-28 11:12:03 +0000

    now resolves lazy strings as subjects
    git-svn-id: http://django-mailer.googlecode.com/svn/trunk@15 09d42984-cf36-0410-8cf8-11b756d0386d

--- mailerdev/mailer/__init__.py
+++ mailerdev/mailer/__init__.py
@@ -1,8 +1,13 @@
+from django.utils.encoding import force_unicode
 
 from mailer.models import Message
 
 # replacement for django.core.mail.send_mail
 
 def send_mail(subject, message, from_address, to_addresses):
+    
+    # need to do this in case subject used lazy version of ugettext
+    subject = force_unicode(subject)
+    
     for to_address in to_addresses:
         Message(to_address=to_address, from_address=from_address, subject=subject, message_body=message).save()
