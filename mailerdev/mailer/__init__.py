afd56d748103d1ec3365e223ff49e9b48607e619
Doug Napoleone <doug.napoleone@gmail.com>
2008-08-06 00:28:19 +0000

    Adding mail_admins replacement using mailer. Adding conditional monkey patch to django.core.handlers.base to use mailer mail_adming on chrashes if settings.MAILER_FOR_CHASH_EMAILS is True.
    git-svn-id: http://django-mailer.googlecode.com/svn/trunk@32 09d42984-cf36-0410-8cf8-11b756d0386d

--- mailerdev/mailer/__init__.py
+++ mailerdev/mailer/__init__.py
@@ -1,13 +1,24 @@
 from django.utils.encoding import force_unicode
-
+from django.conf import settings
 from mailer.models import Message
 
 # replacement for django.core.mail.send_mail
 
 def send_mail(subject, message, from_address, to_addresses):
-    
+
     # need to do this in case subject used lazy version of ugettext
     subject = force_unicode(subject)
-    
+
     for to_address in to_addresses:
         Message(to_address=to_address, from_address=from_address, subject=subject, message_body=message).save()
+
+def mail_admins(subject, message, fail_silently=False):
+    for name, to_address in settings.ADMINS:
+        Message(to_address=to_address,
+                from_address=settings.SERVER_EMAIL,
+                subject=settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
+                message_body=message).save()
+
+if getattr(settings, 'MAILER_FOR_CHASH_EMAILS', False):
+    from django.core.handlers import base
+    base.mail_admins = mail_admins
