c4b9dd3c7261ea4b9d3c3cf599fd00ba5e08fe3f
Brian Rosner <brosner@gmail.com>
2008-09-29 04:18:06 +0000

    Exposed priority on the public API for send_mail and mail_admins.
    git-svn-id: http://django-mailer.googlecode.com/svn/trunk@38 09d42984-cf36-0410-8cf8-11b756d0386d

--- mailerdev/mailer/__init__.py
+++ mailerdev/mailer/__init__.py
@@ -1,23 +1,36 @@
 from django.utils.encoding import force_unicode
 from django.conf import settings
-from mailer.models import Message
+
+PRIORITY_MAPPING = {
+    "high": "1",
+    "medium": "2",
+    "low": "3",
+    "deferred": "4",
+}
 
 # replacement for django.core.mail.send_mail
 
-def send_mail(subject, message, from_address, to_addresses):
-
+def send_mail(subject, message, from_address, to_addresses, priority="medium"):
+    from mailer.models import Message
     # need to do this in case subject used lazy version of ugettext
     subject = force_unicode(subject)
-
+    priority = PRIORITY_MAPPING[priority]
     for to_address in to_addresses:
-        Message(to_address=to_address, from_address=from_address, subject=subject, message_body=message).save()
+        Message(to_address=to_address,
+                from_address=from_address,
+                subject=subject,
+                message_body=message,
+                priority=priority).save()
 
-def mail_admins(subject, message, fail_silently=False):
+def mail_admins(subject, message, fail_silently=False, priority="medium"):
+    from mailer.models import Message
+    priority = PRIORITY_MAPPING[priority]
     for name, to_address in settings.ADMINS:
         Message(to_address=to_address,
                 from_address=settings.SERVER_EMAIL,
                 subject=settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
-                message_body=message).save()
+                message_body=message,
+                priority=priority).save()
 
 if getattr(settings, 'MAILER_FOR_CHASH_EMAILS', False):
     from django.core.handlers import base
