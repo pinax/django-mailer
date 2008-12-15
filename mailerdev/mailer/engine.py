4317f9f0bda9ae163525247d7b840a3024687f50
Brian Rosner <brosner@gmail.com>
2008-12-15 02:41:57 +0000

    Added smtplib.SMTPSenderRefused to the list of exceptions caught to defer the e-mail.
    git-svn-id: http://django-mailer.googlecode.com/svn/trunk@45 09d42984-cf36-0410-8cf8-11b756d0386d

--- mailerdev/mailer/engine.py
+++ mailerdev/mailer/engine.py
@@ -73,7 +73,7 @@ def send_all():
                     MessageLog.objects.log(message, 1) # @@@ avoid using literal result code
                     message.delete()
                     sent += 1
-                except (socket_error, smtplib.SMTPRecipientsRefused, smtplib.SMTPAuthenticationError), err:
+                except (socket_error, smtplib.SMTPSenderRefused, smtplib.SMTPRecipientsRefused, smtplib.SMTPAuthenticationError), err:
                     message.defer()
                     logging.info("message deferred due to failure: %s" % err)
                     MessageLog.objects.log(message, 3, log_message=str(err)) # @@@ avoid using literal result code
