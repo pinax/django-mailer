0ba5c713c3d17215cfe6473f6481d54290d9f070
Jannis Leidel <jannis@leidel.info>
2009-01-12 01:16:30 +0000

    Moved the mailer app one level up
    git-svn-id: http://django-mailer.googlecode.com/svn/trunk@50 09d42984-cf36-0410-8cf8-11b756d0386d

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -1,6 +1,16 @@
 from django.utils.encoding import force_unicode
 from django.conf import settings
 
+VERSION = (0, 1, 0, "pre")
+
+def get_version():
+    if VERSION[3] != "final":
+        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    else:
+        return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
+
+__version__ = get_version()
+
 PRIORITY_MAPPING = {
     "high": "1",
     "medium": "2",
