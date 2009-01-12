6518fb1bf14f8bfdef2ad0a5d01e4a247d615b38
Jannis Leidel <jannis@leidel.info>
2009-01-12 01:18:31 +0000

    Removed unneeded fixtures and development project
    git-svn-id: http://django-mailer.googlecode.com/svn/trunk@51 09d42984-cf36-0410-8cf8-11b756d0386d

--- mailerdev/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^mailerdev/', include('mailerdev.foo.urls')),
-    
-    (r'^admin/(.*)', admin.site.root),
-)
