6db9dd0367fb020c7eabb376886e5fce194c4383
Brian Rosner <brosner@gmail.com>
2010-05-25 08:28:47 -0600

    modified version building to match how Pinax works and set at 0.2a1.dev1

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -1,12 +1,17 @@
-VERSION = (0, 2, 0, "dev", 1)
+VERSION = (0, 2, 0, "a", 1) # following PEP 386
+DEV_N = 1
+
 
 def get_version():
-    if VERSION[3] == "final":
-        return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
-    elif VERSION[3] == "dev":
-        return "%s.%s.%s%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
-    else:
-        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    version = "%s.%s" % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = "%s.%s" % (version, VERSION[2])
+    if VERSION[3] != "f":
+        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
+        if DEV_N:
+            version = "%s.dev%s" % (version, DEV_N)
+    return version
+
 
 __version__ = get_version()
 
