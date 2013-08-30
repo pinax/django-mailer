ef2529d7a2f7a36547c1e4866756b54e1bccd1e6
Alessandro Pagnin <alepane@gmail.com>
2013-08-30 15:54:20 +0200

    - added a cron mode to the management commands

--- mailer/management/commands/retry_deferred.py
+++ mailer/management/commands/retry_deferred.py
@@ -1,4 +1,5 @@
 import logging
+from optparse import make_option
 
 from django.core.management.base import NoArgsCommand
 
@@ -7,8 +8,17 @@ from mailer.models import Message
 
 class Command(NoArgsCommand):
     help = "Attempt to resend any deferred mail."
+    base_options = (
+        make_option('-c', '--cron', default=0, type='int',
+            help='If 1 don\'t print messagges, but only errors.'
+        ),
+    )
+    option_list = NoArgsCommand.option_list + base_options
     
     def handle_noargs(self, **options):
-        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        if options['cron'] == 0:
+            logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        else:
+            logging.basicConfig(level=logging.ERROR, format="%(message)s")
         count = Message.objects.retry_deferred() # @@@ new_priority not yet supported
         logging.info("%s message(s) retried" % count)
--- mailer/management/commands/send_mail.py
+++ mailer/management/commands/send_mail.py
@@ -1,4 +1,5 @@
 import logging
+from optparse import make_option
 
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
@@ -12,9 +13,18 @@ PAUSE_SEND = getattr(settings, "MAILER_PAUSE_SEND", False)
 
 class Command(NoArgsCommand):
     help = "Do one pass through the mail queue, attempting to send all mail."
+    base_options = (
+        make_option('-c', '--cron', default=0, type='int',
+            help='If 1 don\'t print messagges, but only errors.'
+        ),
+    )
+    option_list = NoArgsCommand.option_list + base_options
     
     def handle_noargs(self, **options):
-        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        if options['cron'] == 0:
+            logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        else:
+            logging.basicConfig(level=logging.ERROR, format="%(message)s")
         logging.info("-" * 72)
         # if PAUSE_SEND is turned on don't do anything.
         if not PAUSE_SEND:
