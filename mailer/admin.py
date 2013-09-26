a98815e30b7d06ad5adc1ec4ec891c6a86d5d6fc
Basil Shubin <basil.shubin@gmail.com>
2013-09-26 08:23:16 +0700

    disable ``add`` link in admin (confusing I think)

--- mailer/admin.py
+++ mailer/admin.py
@@ -6,13 +6,22 @@ from mailer.models import Message, DontSendEntry, MessageLog
 class MessageAdmin(admin.ModelAdmin):
     list_display = ["id", "to_addresses", "subject", "when_added", "priority"]
 
+    def has_add_permission(self, request):
+        return False
+
 
 class DontSendEntryAdmin(admin.ModelAdmin):
     list_display = ["to_address", "when_added"]
 
+    def has_add_permission(self, request):
+        return False
+
 
 class MessageLogAdmin(admin.ModelAdmin):
     list_display = ["id", "to_addresses", "subject", "when_attempted", "result"]
+
+    def has_add_permission(self, request):
+        return False
 
 
 admin.site.register(Message, MessageAdmin)
