0ef7bf7851baa17652dbca34981ca6cfc403161d
Basil Shubin <basil.shubin@gmail.com>
2013-11-04 10:05:38 +0700

    bump version number

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 2, 0, "a", 2) # following PEP 386
+VERSION = (0, 2, 0, "a", 3) # following PEP 386
 DEV_N = None
 
 
@@ -31,13 +31,13 @@ def send_mail(subject, message, from_email, recipient_list, priority="medium",
               fail_silently=False, auth_user=None, auth_password=None):
     from django.utils.encoding import force_unicode
     from mailer.models import make_message
-    
+
     priority = PRIORITY_MAPPING[priority]
-    
+
     # need to do this in case subject used lazy version of ugettext
     subject = force_unicode(subject)
     message = force_unicode(message)
-    
+
     make_message(subject=subject,
                  body=message,
                  from_email=from_email,
@@ -55,13 +55,13 @@ def send_html_mail(subject, message, message_html, from_email, recipient_list,
     from django.utils.encoding import force_unicode
     from django.core.mail import EmailMultiAlternatives
     from mailer.models import make_message
-    
+
     priority = PRIORITY_MAPPING[priority]
-    
+
     # need to do this in case subject used lazy version of ugettext
     subject = force_unicode(subject)
     message = force_unicode(message)
-    
+
     msg = make_message(subject=subject,
                        body=message,
                        from_email=from_email,
@@ -87,7 +87,7 @@ def send_mass_mail(datatuple, fail_silently=False, auth_user=None,
 def mail_admins(subject, message, fail_silently=False, connection=None, priority="medium"):
     from django.conf import settings
     from django.utils.encoding import force_unicode
-    
+
     return send_mail(settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
                      message,
                      settings.SERVER_EMAIL,
@@ -97,7 +97,7 @@ def mail_admins(subject, message, fail_silently=False, connection=None, priority
 def mail_managers(subject, message, fail_silently=False, connection=None, priority="medium"):
     from django.conf import settings
     from django.utils.encoding import force_unicode
-    
+
     return send_mail(settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
                      message,
                      settings.SERVER_EMAIL,
