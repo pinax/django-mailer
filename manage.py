6d13ebb4131fc652aecb47f2bc52edda83f56a78
Luke Plant <L.Plant.98@cantab.net>
2014-12-31 11:43:49 +0000

    Fixed manage.py so that it can be used again

--- manage.py
+++ manage.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mailer.test_settings'
-from django.core import management
+DEFAULT_SETTINGS = dict(
+    INSTALLED_APPS=[
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sites",
+        "mailer",
+    ],
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    },
+    SITE_ID=1,
+    SECRET_KEY="notasecret",
+)
+
 if __name__ == "__main__":
+    from django.conf import settings
+    from django.core import management
+    settings.configure(**DEFAULT_SETTINGS)
     management.execute_from_command_line()
