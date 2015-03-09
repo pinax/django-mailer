50073a45c0867890b3d3dc0864e9aa6cf664de3d
Renato Alves <alves.rjc@gmail.com>
2015-03-09 12:55:09 +0000

    Use django's LocMem backend for most tests
    Also avoid local imports and cleanup remaining uses of `global
    sent_messages`.

--- mailer/tests.py
+++ mailer/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.core import mail
+from django.core.mail.backends.locmem import EmailBackend as LocMemEmailBackend
 
 from mailer.models import Message, MessageLog
-from mailer.engine import send_all
+from mailer import send_mail as mailer_send_mail
+from mailer import engine
 
 import smtplib
 
@@ -22,21 +25,19 @@ class TestMailerEmailBackend(object):
         self.outbox.extend(email_messages)
 
 
-class FailingMailerEmailBackend(TestMailerEmailBackend):
+class FailingMailerEmailBackend(LocMemEmailBackend):
     def send_messages(self, email_messages):
         raise smtplib.SMTPSenderRefused(1, "foo", "foo@foo.com")
 
 
 class TestBackend(TestCase):
-
     def test_save_to_db(self):
         """
         Test that using send_mail creates a Message object in DB instead, when EMAIL_BACKEND is set.
         """
-        from django.core.mail import send_mail
         self.assertEqual(Message.objects.count(), 0)
         with self.settings(EMAIL_BACKEND="mailer.backend.DbBackend"):
-            send_mail("Subject", "Body", "sender@example.com", ["recipient@example.com"])
+            mail.send_mail("Subject", "Body", "sender@example.com", ["recipient@example.com"])
             self.assertEqual(Message.objects.count(), 1)
 
 
@@ -46,41 +47,32 @@ class TestSending(TestCase):
         Test that calling "manage.py send_mail" actually sends mail using the
         specified MAILER_EMAIL_BACKEND
         """
-        global sent_messages
-        # Ensure sent_messages is empty
-        del sent_messages[:]
-        from mailer import send_mail
         with self.settings(MAILER_EMAIL_BACKEND="mailer.tests.TestMailerEmailBackend"):
-            send_mail("Subject", "Body", "sender@example.com", ["recipient@example.com"])
+            mailer_send_mail("Subject", "Body", "sender1@example.com", ["recipient@example.com"])
             self.assertEqual(Message.objects.count(), 1)
-            self.assertEqual(len(sent_messages), 0)
-            from mailer.engine import send_all  # noqa
-            send_all()
-            self.assertEqual(len(sent_messages), 1)
+            self.assertEqual(len(TestMailerEmailBackend.outbox), 0)
+            engine.send_all()
+            self.assertEqual(len(TestMailerEmailBackend.outbox), 1)
             self.assertEqual(Message.objects.count(), 0)
             self.assertEqual(MessageLog.objects.count(), 1)
 
     def test_retry_deferred(self):
-        global sent_messages
-        # Ensure sent_messages is empty
-        del sent_messages[:]
-
-        from mailer import send_mail
         with self.settings(MAILER_EMAIL_BACKEND="mailer.tests.FailingMailerEmailBackend"):
-            send_mail("Subject", "Body", "sender@example.com", ["recipient@example.com"])
-            send_all()
+            mailer_send_mail("Subject", "Body", "sender2@example.com", ["recipient@example.com"])
+            engine.send_all()
             self.assertEqual(Message.objects.count(), 1)
             self.assertEqual(Message.objects.deferred().count(), 1)
 
-        with self.settings(MAILER_EMAIL_BACKEND="mailer.tests.TestMailerEmailBackend"):
-            send_all()
-            self.assertEqual(len(sent_messages), 0)
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
+
+            engine.send_all()
+            self.assertEqual(len(mail.outbox), 0)
             # Should not have sent the deferred ones
             self.assertEqual(Message.objects.count(), 1)
             self.assertEqual(Message.objects.deferred().count(), 1)
 
             # Now mark them for retrying
             Message.objects.retry_deferred()
-            send_all()
-            self.assertEqual(len(sent_messages), 1)
+            engine.send_all()
+            self.assertEqual(len(mail.outbox), 1)
             self.assertEqual(Message.objects.count(), 0)
