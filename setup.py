95509058da50240fb1dd7f00e779c62a33eb8c0e
Renato Alves <alves.rjc@gmail.com>
2015-03-13 14:54:01 +0000

    setup.py didn't define dependencies correctly

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Framework :: Django",
     ],
-    install_requires=[
+    setup_requires=[
         'Django >= 1.4',
         'lockfile >= 0.8',
         ],
