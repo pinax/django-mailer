ebeaf4c038c1ff075844f99bd82cf1e8be560b55
Renato Alves <alves.rjc@gmail.com>
2015-03-15 21:26:26 +0000

    Test delivery of messages
    * Sending HTML messages
    * Sending messages to admins
    * Sending messages to managers
    * Sending mass mail messages
    * Ensure emails in DoNotSend are not delivered
    * Ensure proper behavior with Message and MessageLog objects
    * Ensure email_to_db and db_to_email are symmetric and work as expected

--- mailer/tests.py
+++ mailer/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.core import mail
 from django.core.mail.backends.locmem import EmailBackend as LocMemEmailBackend
+from django.utils.timezone import now as datetime_now
 
-from mailer.models import Message, MessageLog
+from mailer.models import Message, MessageLog, DontSendEntry, db_to_email, email_to_db
 import mailer
 from mailer import engine
 
@@ -93,6 +94,117 @@ class TestSending(TestCase):
                 self.assertRaises(StopIteration, engine.send_loop)
                 send.assert_called_once()
 
+    def test_send_html(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
+            mailer.send_html_mail("Subject", "Body", "<html><body>Body</body></html>",
+                                  "htmlsender1@example.com", ["recipient@example.com"], "high")
+
+            # Ensure deferred was not deleted
+            self.assertEqual(Message.objects.count(), 1)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            engine.send_all()
+
+            self.assertEqual(len(mail.outbox), 1)
+            sent = mail.outbox[0]
+
+            # Default "plain text"
+            self.assertEqual(sent.body, "Body")
+            self.assertEqual(sent.content_subtype, "plain")
+
+            # Alternative "text/html"
+            self.assertEqual(sent.alternatives[0],
+                             ("<html><body>Body</body></html>", "text/html"))
+
+    def test_send_mass_mail(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
+            mails = (
+                ("Subject", "Body", "mass0@example.com", ["recipient0@example.com"]),
+                ("Subject", "Body", "mass1@example.com", ["recipient1@example.com"]),
+                ("Subject", "Body", "mass2@example.com", ["recipient2@example.com"]),
+                ("Subject", "Body", "mass3@example.com", ["recipient3@example.com"]),
+            )
+
+            mailer.send_mass_mail(mails)
+
+            self.assertEqual(Message.objects.count(), 4)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            engine.send_all()
+
+            self.assertEqual(Message.objects.count(), 0)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            self.assertEqual(len(mail.outbox), 4)
+            for i, sent in enumerate(mail.outbox):
+                # Default "plain text"
+                self.assertEqual(sent.from_email, "mass{0}@example.com".format(i))
+                self.assertEqual(sent.to, ["recipient{0}@example.com".format(i)])
+
+    def test_mail_admins(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend", ADMINS=(("Test", "testadmin@example.com"),)):  # noqa
+            mailer.mail_admins("Subject", "Admin Body")
+
+            self.assertEqual(Message.objects.count(), 1)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            engine.send_all()
+
+            self.assertEqual(Message.objects.count(), 0)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            self.assertEqual(len(mail.outbox), 1)
+            sent = mail.outbox[0]
+
+            # Default "plain text"
+            self.assertEqual(sent.body, "Admin Body")
+            self.assertEqual(sent.to, ["testadmin@example.com"])
+
+    def test_mail_managers(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend", MANAGERS=(("Test", "testmanager@example.com"),)):  # noqa
+            mailer.mail_managers("Subject", "Manager Body")
+
+            self.assertEqual(Message.objects.count(), 1)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            engine.send_all()
+
+            self.assertEqual(Message.objects.count(), 0)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            self.assertEqual(len(mail.outbox), 1)
+            sent = mail.outbox[0]
+
+            # Default "plain text"
+            self.assertEqual(sent.body, "Manager Body")
+            self.assertEqual(sent.to, ["testmanager@example.com"])
+
+    def test_blacklisted_emails(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
+            now = datetime_now()
+            obj = DontSendEntry.objects.create(to_address="nogo@example.com", when_added=now)
+            self.assertTrue(obj.to_address, "nogo@example.com")
+
+            mailer.send_mail("Subject", "GoBody", "send1@example.com", ["go@example.com"])
+            mailer.send_mail("Subject", "NoGoBody", "send2@example.com", ["nogo@example.com"])
+
+            self.assertEqual(Message.objects.count(), 2)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            engine.send_all()
+
+            # All messages are processed
+            self.assertEqual(Message.objects.count(), 0)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+
+            # but only one should get sent
+            self.assertEqual(len(mail.outbox), 1)
+            sent = mail.outbox[0]
+
+            # Default "plain text"
+            self.assertEqual(sent.body, "GoBody")
+            self.assertEqual(sent.to, ["go@example.com"])
+
 
 class TestLockNormal(TestCase):
     def setUp(self):
@@ -171,7 +283,6 @@ class TestLockTimeout(TestCase):
 class TestPrioritize(TestCase):
     def test_prioritize(self):
         with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
-            # 5 normal emails scheduled for delivery
             mailer.send_mail("Subject", "Body", "prio1@example.com", ["recipient@example.com"], "high")
             mailer.send_mail("Subject", "Body", "prio2@example.com", ["recipient@example.com"], "medium")
             mailer.send_mail("Subject", "Body", "prio3@example.com", ["recipient@example.com"], "low")
@@ -244,3 +355,81 @@ class TestPrioritize(TestCase):
             # Ensure deferred was not deleted
             self.assertEqual(Message.objects.count(), 1)
             self.assertEqual(Message.objects.deferred().count(), 1)
+
+
+class TestMessages(TestCase):
+    def test_message(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
+            mailer.send_mail("Subject Msg", "Body", "msg1@example.com", ["rec1@example.com"])
+
+            self.assertEqual(Message.objects.count(), 1)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+            self.assertEqual(MessageLog.objects.count(), 0)
+
+            msg = Message.objects.all()[0]
+
+            self.assertEqual(msg.email.from_email, "msg1@example.com")
+            self.assertEqual(msg.to_addresses, ["rec1@example.com"])
+            self.assertEqual(msg.subject, "Subject Msg")
+
+            # Fake a msg stored in DB with invalid data
+            msg.message_data = ""
+
+            self.assertEqual(msg.to_addresses, [])
+            self.assertEqual(msg.subject, "")
+
+            msg.save()
+
+            engine.send_all()
+
+            self.assertEqual(Message.objects.count(), 0)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+            # Delivery should discard broken messages
+            self.assertEqual(MessageLog.objects.count(), 0)
+
+    def test_message_log(self):
+        with self.settings(MAILER_EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"):
+            mailer.send_mail("Subject Log", "Body", "log1@example.com", ["1gol@example.com"])
+
+            self.assertEqual(Message.objects.count(), 1)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+            self.assertEqual(MessageLog.objects.count(), 0)
+
+            engine.send_all()
+
+            self.assertEqual(Message.objects.count(), 0)
+            self.assertEqual(Message.objects.deferred().count(), 0)
+            self.assertEqual(MessageLog.objects.count(), 1)
+
+            log = MessageLog.objects.all()[0]
+
+            self.assertEqual(log.email.from_email, "log1@example.com")
+            self.assertEqual(log.to_addresses, ["1gol@example.com"])
+            self.assertEqual(log.subject, "Subject Log")
+
+            # Fake a log entry without email
+            log.message_data = ""
+
+            self.assertEqual(log.to_addresses, [])
+            self.assertEqual(log.subject, "")
+
+
+class TestDbToEmail(TestCase):
+    def test_db_to_email(self):
+        # Empty/Invalid content
+        self.assertEqual(db_to_email(""), None)
+        self.assertEqual(db_to_email(None), None)
+
+        # Other objects which should be returned as-is
+        data = "Hello Email"
+        self.assertEqual(db_to_email(email_to_db(data)), data)
+
+        data = ["Test subject", "Test body", "testsender@example.com", ["testrec@example.com"]]
+        self.assertEqual(db_to_email(email_to_db(data)), data)
+
+        email = mail.EmailMessage(*data)
+        converted_email = db_to_email(email_to_db(email))
+        self.assertEqual(converted_email.body, email.body)
+        self.assertEqual(converted_email.subject, email.subject)
+        self.assertEqual(converted_email.from_email, email.from_email)
+        self.assertEqual(converted_email.to, email.to)
