8263926c383905ed48ab1b013176ac8b6663c053
Luke Plant <L.Plant.98@cantab.net>
2016-06-29 15:48:18 +0300

    Version bump

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import warnings
 
-VERSION = (1, 2, 0, "final", 0)  # following PEP 386
+VERSION = (1, 2, 1, "final", 0)  # following PEP 386
 DEV_N = None
 
 
