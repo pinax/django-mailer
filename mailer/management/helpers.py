344611286736eae4ef525b86129040ad3cc60d17
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2016-07-05 19:57:30 +0200

    Fixed #65 Added Django 1.10 support

--- mailer/management/helpers.py
+++ mailer/management/helpers.py
@@ -1,17 +1,16 @@
 from optparse import make_option
-
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 help_msg = "If 1 don't print messagges, but only errors."
 
 
-if NoArgsCommand.option_list:
+if hasattr(BaseCommand, 'option_list') and BaseCommand.option_list:
     # pre django 1.8; use optparse
     class CronArgMixin(object):
         base_options = (
             make_option('-c', '--cron', default=0, type='int', help=help_msg),
         )
-        option_list = NoArgsCommand.option_list + base_options
+        option_list = BaseCommand.option_list + base_options
 else:
     # django 1.8+; use argparse
     class CronArgMixin(object):
