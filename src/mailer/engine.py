8e01a7b3cb10a72367f37b82d1a8a22e63d544c4
Jaap Roes <jroes@leukeleu.nl>
2021-01-13 10:44:33 +0100

    Minor optimization to the database query in send_loop

--- src/mailer/engine.py
+++ src/mailer/engine.py
@@ -267,7 +267,7 @@ def send_loop():
     """
 
     while True:
-        while not Message.objects.all():
+        while not Message.objects.all().exists():
             logger.debug("sleeping for %s seconds before checking queue again" % EMPTY_QUEUE_SLEEP)
             time.sleep(EMPTY_QUEUE_SLEEP)
         send_all()
