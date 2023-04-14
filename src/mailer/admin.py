5aec9a162d6b90eebeb9e53f14175181821832b8
Daniel Lima <daniel.lima@ifma.edu.br>
2023-04-14 10:47:41 -0300

    Add subject to messagelog filter

--- src/mailer/admin.py
+++ src/mailer/admin.py
@@ -44,7 +44,7 @@ class MessageLogAdmin(MessageAdminMixin, admin.ModelAdmin):
     list_filter = ["result", "account"]
     date_hierarchy = "when_attempted"
     readonly_fields = ['plain_text_body', 'message_id']
-    search_fields = ['message_id', 'account', 'to']
+    search_fields = ['message_id', 'account', 'to', 'subject']
 
 
 admin.site.register(Message, MessageAdmin)
