752c9e06baaa49ee5afa6e74970dd6c78fdf599f
Luke Plant <luke@lukeplant.me.uk>
2025-11-26 21:29:43 +0000

    Switch to uv

--- release.sh
+++ release.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+# Tests to ensure we don't really break something:
+uv sync
+pre-commit run --all --all-files || exit 1
+pytest || exit 1
+
 umask 000
 rm -rf build dist
 git ls-tree --full-tree --name-only -r HEAD | xargs chmod ugo+r
 find src docs tests -type d | xargs chmod ugo+rx
 
-python setup.py sdist bdist_wheel || exit 1
+uv build --sdist --wheel || exit 1
+uv publish  || exit 1
 
-VERSION=$(python setup.py --version) || exit 1
-twine upload dist/django_mailer-$VERSION-py3-none-any.whl dist/django-mailer-$VERSION.tar.gz || exit 1
+VERSION=$(uv pip show django-mailer | grep 'Version: ' | cut -f 2 -d ' ' | tr -d '\n') || exit 1
+
+git tag $VERSION || exit 1
+git push || exit 1
+git push --tags || exit 1
